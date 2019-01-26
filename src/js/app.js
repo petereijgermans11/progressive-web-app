7bf2dfc30e7d176f29ce940defceddd8bd42659a
raduenuca <raduenuca@gmail.com>
2019-01-26 21:31:30 +0200

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        const base = document.querySelector('base');
+        let baseUrl = base && base.href || '';
+
+        if (!baseUrl.endsWith('/')) {
+            baseUrl = `${baseUrl}/`;
+        }
+
+        navigator.serviceWorker.register(`${baseUrl}sw.js`)
             .then( registration => {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
