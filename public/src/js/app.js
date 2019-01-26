495e4fd60a13a5c9212228ee983a52c3aa0079cc
raduenuca <raduenuca@gmail.com>
2019-01-26 20:54:41 +0200

    Added base path for GitHub Pages

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('/fe-guild-2019-pwa/public/sw.js')
             .then( registration => {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
