726d044d755c8163ca16f8e48eebdf74e102e4ec
raduenuca <raduenuca@gmail.com>
2019-01-27 09:13:17 +0200

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,54 @@
+const manifest = {
+    "name": "Progressive Selfies",
+    "short_name": "PWA Selfies",
+    "icons": [
+        {
+            "src": "src/images/icons/app-icon-48x48.png",
+            "type": "image/png",
+            "sizes": "48x48"
+        },
+        {
+            "src": "src/images/icons/app-icon-96x96.png",
+            "type": "image/png",
+            "sizes": "96x96"
+        },
+        {
+            "src": "src/images/icons/app-icon-144x144.png",
+            "type": "image/png",
+            "sizes": "144x144"
+        },
+        {
+            "src": "src/images/icons/app-icon-192x192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+        },
+        {
+            "src": "src/images/icons/app-icon-256x256.png",
+            "type": "image/png",
+            "sizes": "256x256"
+        },
+        {
+            "src": "src/images/icons/app-icon-384x384.png",
+            "type": "image/png",
+            "sizes": "384x384"
+        },
+        {
+            "src": "src/images/icons/app-icon-512x512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+        }
+    ],
+    "start_url": "index.html",
+    //"scope": ".",
+    "display": "standalone",
+    "orientation": "portrait-primary",
+    "background_color": "#fff",
+    "theme_color": "#3f51b5",
+    "description": "Take selfies PWA style.",
+    "dir": "ltr",
+    "lang": "en-US"
+};
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         const base = document.querySelector('base');
@@ -6,6 +57,17 @@ if ('serviceWorker' in navigator) {
         if (!baseUrl.endsWith('/')) {
             baseUrl = `${baseUrl}/`;
         }
+
+        manifest['start_url'] = `${baseUrl}${manifest['start_url']}`;
+
+        manifest.icons.forEach(icon => {
+            icon.src = `${baseUrl}${icon.src}`;
+        });
+
+        const stringManifest = JSON.stringify(manifest);
+        const blob = new Blob([stringManifest], {type: 'application/json'});
+        const manifestURL = URL.createObjectURL(blob);
+        document.querySelector('#manifestPlaceholder').setAttribute('href', manifestURL);
 
         navigator.serviceWorker.register(`${baseUrl}sw.js`)
             .then( registration => {
