eeaf17a93d45ecab7973bbbe61124a0c97e05e85
raduenuca <raduenuca@gmail.com>
2019-01-27 12:47:38 +0200

    Added deferred install prompt

--- fe-guild-2019-pwa/src/js/feed.js
+++ fe-guild-2019-pwa/src/js/feed.js
@@ -2,7 +2,30 @@ const shareImageButton = document.querySelector('#share-image-button');
 const createPostArea = document.querySelector('#create-post');
 const closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
 
-const openCreatePostModal = () => createPostArea.style.transform = 'translateY(0)';
+const openCreatePostModal = () => {
+    createPostArea.style.transform = 'translateY(0)';
+
+    if(deferredPrompt) {
+        deferredPrompt.prompt();
+
+        // Determine the user's choice - returned as a Promise
+        deferredPrompt.userChoice.then(result => {
+            console.log(result.outcome);
+
+            // Based on the user's choice, decide how to proceed
+            if (result.outcome === 'dismissed') {
+                // Send to analytics
+                console.log('User cancelled installation');
+            } else {
+                // Send to analytics
+                console.log('User added to home screen');
+            }
+        });
+
+        deferredPrompt = null;
+    }
+};
+
 const closeCreatePostModal = () => createPostArea.style.transform = 'translateY(100vh)';
 
 shareImageButton.addEventListener('click', openCreatePostModal);
