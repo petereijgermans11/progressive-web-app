ad0c348027cc8263a251022025763a35e2ca3aed
petereijgermans11 <p.eijgermans11@gmail.com>
2019-01-27 12:47:38 +0200

    Added deferred install prompt

--- fe-guild-2019-pwa/src/js/app.js
+++ fe-guild-2019-pwa/src/js/app.js
@@ -1,82 +1,95 @@
 const manifest = {
-    "name": "Progressive Selfies",
-    "short_name": "PWA Selfies",
-    "icons": [
+    'name': 'Progressive Selfies',
+    'short_name': 'PWA Selfies',
+    'icons': [
         {
-            "src": "src/images/icons/app-icon-48x48.png",
-            "type": "image/png",
-            "sizes": "48x48"
+            'src': 'src/images/icons/app-icon-48x48.png',
+            'type': 'image/png',
+            'sizes': '48x48'
         },
         {
-            "src": "src/images/icons/app-icon-96x96.png",
-            "type": "image/png",
-            "sizes": "96x96"
+            'src': 'src/images/icons/app-icon-96x96.png',
+            'type': 'image/png',
+            'sizes': '96x96'
         },
         {
-            "src": "src/images/icons/app-icon-144x144.png",
-            "type": "image/png",
-            "sizes": "144x144"
+            'src': 'src/images/icons/app-icon-144x144.png',
+            'type': 'image/png',
+            'sizes': '144x144'
         },
         {
-            "src": "src/images/icons/app-icon-192x192.png",
-            "type": "image/png",
-            "sizes": "192x192"
+            'src': 'src/images/icons/app-icon-192x192.png',
+            'type': 'image/png',
+            'sizes': '192x192'
         },
         {
-            "src": "src/images/icons/app-icon-256x256.png",
-            "type": "image/png",
-            "sizes": "256x256"
+            'src': 'src/images/icons/app-icon-256x256.png',
+            'type': 'image/png',
+            'sizes': '256x256'
         },
         {
-            "src": "src/images/icons/app-icon-384x384.png",
-            "type": "image/png",
-            "sizes": "384x384"
+            'src': 'src/images/icons/app-icon-384x384.png',
+            'type': 'image/png',
+            'sizes': '384x384'
         },
         {
-            "src": "src/images/icons/app-icon-512x512.png",
-            "type": "image/png",
-            "sizes": "512x512"
+            'src': 'src/images/icons/app-icon-512x512.png',
+            'type': 'image/png',
+            'sizes': '512x512'
         }
     ],
-    "start_url": "index.html",
+    //"start_url": "index.html",
     //"scope": ".",
-    "display": "standalone",
-    "orientation": "portrait-primary",
-    "background_color": "#fff",
-    "theme_color": "#3f51b5",
-    "description": "Take selfies PWA style.",
-    "dir": "ltr",
-    "lang": "en-US"
+    'display': 'standalone',
+    'orientation': 'portrait-primary',
+    'background_color': '#fff',
+    'theme_color': '#3f51b5',
+    'description': 'Take selfies PWA style.',
+    'dir': 'ltr',
+    'lang': 'en-US'
 };
+let deferredPrompt;
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        const base = document.querySelector('base');
-        let baseUrl = base && base.href || '';
+window.addEventListener('load', () => {
+    const base = document.querySelector('base');
+    let baseUrl = base && base.href || '';
 
-        if (!baseUrl.endsWith('/')) {
-            baseUrl = `${baseUrl}/`;
-        }
+    if (!baseUrl.endsWith('/')) {
+        baseUrl = `${baseUrl}/`;
+    }
 
-        manifest['start_url'] = `${baseUrl}${manifest['start_url']}`;
+    manifest['start_url'] = `${baseUrl}index.html`;
 
-        manifest.icons.forEach(icon => {
-            icon.src = `${baseUrl}${icon.src}`;
-        });
-
-        const stringManifest = JSON.stringify(manifest);
-        const blob = new Blob([stringManifest], {type: 'application/json'});
-        const manifestURL = URL.createObjectURL(blob);
-        document.querySelector('#manifestPlaceholder').setAttribute('href', manifestURL);
-
-        navigator.serviceWorker.register(`${baseUrl}sw.js`)
-            .then( registration => {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        })
-        .catch(err => {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
+    manifest.icons.forEach(icon => {
+        icon.src = `${baseUrl}${icon.src}`;
     });
-}
+
+    const stringManifest = JSON.stringify(manifest);
+    const blob = new Blob([stringManifest], {type: 'application/json'});
+    const manifestURL = URL.createObjectURL(blob);
+    document.querySelector('#manifestPlaceholder').setAttribute('href', manifestURL);
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register(`${baseUrl}sw.js`)
+            .then(registration => {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch(err => {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    }
+});
+
+window.addEventListener('beforeinstallprompt', event => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    event.preventDefault();
+
+    console.log('beforeinstallprompt fired');
+
+    // Stash the event so it can be triggered later.
+    deferredPrompt = event;
+
+    return false;
+});
