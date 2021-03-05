fe20752d956d27bde6f6f1af6e76f6678a0e2182
Eijgermans, P.B.J. (Peter) <p.eijgermans11@gmail.com>
2021-03-05 10:32:38 +0100

    added function urlBase64ToUint8Array for applicationServerKey
    added function urlBase64ToUint8Array for applicationServerKey

--- pwa-article/pwa-app-native-features-push-api-init/public/src/js/utility.js
+++ pwa-article/pwa-app-native-features-push-api-init/public/src/js/utility.js
@@ -1,4 +1,3 @@
-
 // TODO: Change this with your own local IP (either localhost/127.0.0.1) or the IP assigned by the phone hot spot
 const SERVER_URL = 'http://localhost:3000';
 const API_URL = `${SERVER_URL}/selfies`;
@@ -54,13 +53,28 @@ const deleteItemFromData = (storeName, id) => {
 };
 
 const dataURItoBlob = dataURI => {
-  const byteString = atob(dataURI.split(',')[1]);
-  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-  const ab = new ArrayBuffer(byteString.length);
-  const ia = new Uint8Array(ab);
-  for (let i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-  }
-  const blob = new Blob([ab], {type: mimeString});
-  return blob;
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([ab], {type: mimeString});
+    return blob;
+};
+
+const urlBase64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 };
